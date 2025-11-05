1ddb90be99931ae5d0729c9e765d579703ee014d
[dyad] <vqtrung357@gmail.com>
2025-11-05 13:04:09 +0700

    Reverted all changes back to version 32fcc60eda07f00c338a201a54f920a2599db9a9

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
+import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
-import { WalletButton } from "./WalletButton";
 
 export const Header = () => {
   return (
@@ -32,7 +32,8 @@ export const Header = () => {
         >
           About
         </Link>
-        <WalletButton />
+        <Button>Connect Wallet</Button>
       </nav>
     </header>
   );
+};
